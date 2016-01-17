93258202931db911a2effacb08b0c3261ea60f44
Alun-Price <thealzer@gmail.com>
2016-01-17 14:20:58 +0000

    update first commit

---
+++ setup-libs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# call this script from the command line like this:
+# $ ./setup-libs.sh
+
+# Install commands
+sudo apt-get -y install libjpeg-dev zlib1g-dev libmysqlclient-dev
+# add more apt-get install lines below if you need more libs installed
